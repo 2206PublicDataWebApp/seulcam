191897a9d364fcdbcf9d35c0078818aa65143d9c
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-10-25 09:43:29 +0900

    221025 commit

--- src/main/java/com/kh/seulcam/product/controller/ProductController.java
+++ src/main/java/com/kh/seulcam/product/controller/ProductController.java
@@ -21,13 +21,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.google.code.geocoder.Geocoder;
-import com.google.code.geocoder.GeocoderRequestBuilder;
-import com.google.code.geocoder.model.GeocodeResponse;
-import com.google.code.geocoder.model.GeocoderRequest;
-import com.google.code.geocoder.model.GeocoderResult;
-import com.google.code.geocoder.model.GeocoderStatus;
-import com.google.code.geocoder.model.LatLng;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.kh.seulcam.product.domain.Brand;
@@ -192,25 +185,25 @@ public class ProductController {
 		
 		String location = req.getParameter("addr");
         
-	      Geocoder geocoder = new Geocoder();
-	      // setAddress : 변환하려는 주소 (경기도 성남시 분당구 등)
-	      // setLanguate : 인코딩 설정
-	      GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(location).setLanguage("ko").getGeocoderRequest();
-	      GeocodeResponse geocoderResponse;
-	      geocoderResponse = geocoder.geocode(geocoderRequest);
-	            if (geocoderResponse.getStatus() == GeocoderStatus.OK & !geocoderResponse.getResults().isEmpty()) {
-	                  GeocoderResult geocoderResult=geocoderResponse.getResults().iterator().next();
-	                  LatLng latitudeLongitude = geocoderResult.getGeometry().getLocation();
-	                                           
-	                  Float[] coords = new Float[2];
-	                  coords[0] = latitudeLongitude.getLat().floatValue();
-	                  coords[1] = latitudeLongitude.getLng().floatValue();
-	                  String coordStr = Float.toString(coords[0])+"&"+Float.toString(coords[1]);
-	                  
-	                  response.setCharacterEncoding("UTF-8");
-	              response.setContentType("text/xml");
-	              response.getWriter().write(coordStr); // 응답결과반환
-	            }
+//	      Geocoder geocoder = new Geocoder();
+//	      // setAddress : 변환하려는 주소 (경기도 성남시 분당구 등)
+//	      // setLanguate : 인코딩 설정
+//	      GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(location).setLanguage("ko").getGeocoderRequest();
+//	      GeocodeResponse geocoderResponse;
+//	      geocoderResponse = geocoder.geocode(geocoderRequest);
+//	            if (geocoderResponse.getStatus() == GeocoderStatus.OK & !geocoderResponse.getResults().isEmpty()) {
+//	                  GeocoderResult geocoderResult=geocoderResponse.getResults().iterator().next();
+//	                  LatLng latitudeLongitude = geocoderResult.getGeometry().getLocation();
+//	                                           
+//	                  Float[] coords = new Float[2];
+//	                  coords[0] = latitudeLongitude.getLat().floatValue();
+//	                  coords[1] = latitudeLongitude.getLng().floatValue();
+//	                  String coordStr = Float.toString(coords[0])+"&"+Float.toString(coords[1]);
+//	                  
+//	                  response.setCharacterEncoding("UTF-8");
+//	              response.setContentType("text/xml");
+//	              response.getWriter().write(coordStr); // 응답결과반환
+//	            }
 		return null;
 		
 	            
